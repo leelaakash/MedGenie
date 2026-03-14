891600b73efa74b7afe9b617fd1d603c1d1e86af
leelaakash <leelaakash7@gmail.com>
2026-03-14 15:49:48 +0530

    backend: document async error handling utility

--- Server/utils/catchAsync.js
+++ Server/utils/catchAsync.js
@@ -1,3 +1,5 @@
+// Utility wrapper to handle async errors in Express controllers
+// Prevents repetitive try-catch blocks in route handlers
 module.exports = fn => {
     return (req, res, next) => {
       fn(req, res, next).catch(next);
